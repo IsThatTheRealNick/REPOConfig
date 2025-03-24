1403a29c67aaec58707c598e638908a583d555ed
Nick <nickkisbetterthanyou@gmail.com>
2025-03-24 17:55:16 -0400

    Reinstated REPOConfigEntryAttribute
    While its not used, I have no reason to remove it in case old mods break

--- REPOConfig/Entry.cs
+++ REPOConfig/Entry.cs
@@ -7,64 +7,63 @@ using HarmonyLib;
 using MonoMod.RuntimeDetour;
 using UnityEngine;
 
-namespace REPOConfig
+namespace REPOConfig;
+
+[BepInPlugin("nickklmao.repoconfig", MOD_NAME, "1.1.7"), BepInDependency("nickklmao.menulib", "2.0.0")]
+internal sealed class Entry : BaseUnityPlugin
 {
-    [BepInPlugin("nickklmao.repoconfig", MOD_NAME, "1.1.7"), BepInDependency("nickklmao.menulib", "2.0.0")]
-    internal sealed class Entry : BaseUnityPlugin
+    private const string MOD_NAME = "REPOConfig";
+
+    internal static readonly ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(MOD_NAME);
+        
+    internal static ConfigEntry<bool> showDescriptions;
+        
+    internal static ConfigEntry<float> descriptionScrollSpeed;
+        
+    private static ConfigEntry<bool> showInGame;
+        
+    private static void MenuPageMain_StartHook(Action<MenuPageMain> orig, MenuPageMain self)
     {
-        private const string MOD_NAME = "REPOConfig";
-
-        internal static readonly ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(MOD_NAME);
-        
-        internal static ConfigEntry<bool> showDescriptions;
-        
-        internal static ConfigEntry<float> descriptionScrollSpeed;
-        
-        private static ConfigEntry<bool> showInGame;
-        
-        private static void MenuPageMain_StartHook(Action<MenuPageMain> orig, MenuPageMain self)
-        {
-            var childrenInOrder = self.transform.Cast<Transform>()
-                .Where(transform => transform.name.Contains("Menu Button"))
-                .OrderByDescending(transform => transform.localPosition.y);
+        var childrenInOrder = self.transform.Cast<Transform>()
+            .Where(transform => transform.name.Contains("Menu Button"))
+            .OrderByDescending(transform => transform.localPosition.y);
             
-            var yPosition = 224f;
-            foreach (var child in childrenInOrder)
-            {
-                if (child.name.Contains("Quit"))
-                    yPosition -= 34;
-                
-                child.localPosition = child.localPosition with { y = yPosition};
+        var yPosition = 224f;
+        foreach (var child in childrenInOrder)
+        {
+            if (child.name.Contains("Quit"))
                 yPosition -= 34;
-            }
-            
-            orig.Invoke(self);
-        }
-        
-        private void Awake()
-        {
-            showDescriptions = Config.Bind("General", "Show Descriptions", true);
-
-            showDescriptions.SettingChanged += (_, _) => {
-                var modButton = ConfigMenu.lastClickedModButton;
                 
-                if (!modButton)
-                    return;
-
-                ConfigMenu.lastClickedModButton = null;
-                modButton.onClick.Invoke();
-            };
-            
-            descriptionScrollSpeed = Config.Bind("General", "Description Scroll Speed", .15f, new ConfigDescription("How fast descriptions scroll. (Seconds per character)", new AcceptableValueRange<float>(0.1f, 2f)));
-            showInGame = Config.Bind("General", "Show In Game", true, new ConfigDescription(string.Empty, null, "HideFromREPOConfig"));
-
-            if (!showInGame.Value)
-                return;
-            
-            logger.LogDebug("Hooking `MenuPageMain.Start`");
-            new Hook(AccessTools.Method(typeof(MenuPageMain), "Start"), MenuPageMain_StartHook);
-
-            ConfigMenu.Initialize();
+            child.localPosition = child.localPosition with { y = yPosition};
+            yPosition -= 34;
         }
+            
+        orig.Invoke(self);
+    }
+        
+    private void Awake()
+    {
+        showDescriptions = Config.Bind("General", "Show Descriptions", true);
+
+        showDescriptions.SettingChanged += (_, _) => {
+            var modButton = ConfigMenu.lastClickedModButton;
+                
+            if (!modButton)
+                return;
+
+            ConfigMenu.lastClickedModButton = null;
+            modButton.onClick.Invoke();
+        };
+            
+        descriptionScrollSpeed = Config.Bind("General", "Description Scroll Speed", .15f, new ConfigDescription("How fast descriptions scroll. (Seconds per character)", new AcceptableValueRange<float>(0.1f, 2f)));
+        showInGame = Config.Bind("General", "Show In Game", true, new ConfigDescription(string.Empty, null, "HideFromREPOConfig"));
+
+        if (!showInGame.Value)
+            return;
+            
+        logger.LogDebug("Hooking `MenuPageMain.Start`");
+        new Hook(AccessTools.Method(typeof(MenuPageMain), "Start"), MenuPageMain_StartHook);
+
+        ConfigMenu.Initialize();
     }
 }
---
+++ REPOConfig/REPOConfigEntryAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace REPOConfig;
+ 
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property), Obsolete("This attribute is not used and will be removed eventually.")]
+public class REPOConfigEntryAttribute : Attribute
+
+{
+    public REPOConfigEntryAttribute(string displayName) {}
+    
+    public REPOConfigEntryAttribute(string displayName, int min, int max, string prefix = "", string postfix = "") { }
+    
+    public REPOConfigEntryAttribute(string displayName, float min, float max, string prefix = "", string postfix = "") {}
+    
+    public REPOConfigEntryAttribute(string displayName, float min, float max, int precision = 2, string prefix = "", string postfix = "") { }
+    
+    public REPOConfigEntryAttribute(string displayName, string prefix = "", string postfix = "") { }
+    
+    public REPOConfigEntryAttribute(string displayName, params string[] customOptions) { }
+}
